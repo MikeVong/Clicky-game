d33fdd3b48452d57241384cbc399587ca0349c39
Mike Vong <mv910@hotmail.com>
2020-06-22 15:29:24 -0700

    Updates

---
+++ precache-manifest.089b45f9cb13eb2a96f89f6cc5c0e3c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1975dfa171b384b1faea2f52a692497",
+    "url": "/Clicky-game/index.html"
+  },
+  {
+    "revision": "f8d25c625415ca5b429c",
+    "url": "/Clicky-game/static/css/main.3827e7ac.chunk.css"
+  },
+  {
+    "revision": "714359ac221eb521c890",
+    "url": "/Clicky-game/static/js/2.7d72e063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Clicky-game/static/js/2.7d72e063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8d25c625415ca5b429c",
+    "url": "/Clicky-game/static/js/main.95ddfd93.chunk.js"
+  },
+  {
+    "revision": "2097f2d8a164c0f77876",
+    "url": "/Clicky-game/static/js/runtime-main.e8a2045f.js"
+  }
+]);
